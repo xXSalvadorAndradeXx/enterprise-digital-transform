f6f8969c9586971bfe6393ec9c845d33d8c325c9
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-04-16 20:04:07 -0600

    Servidor ERP montado

---
+++ my-app/index.ts
@@ -0,0 +1,15 @@
+import figlet from 'figlet';
+import index from './index.html'; 
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": index, 
+    "/figlet": () => {
+      const body = figlet.textSync('Bun!');
+      return new Response(body);
+    }
+  }
+});
+
+console.log(`Listening on ${server.url}`);
