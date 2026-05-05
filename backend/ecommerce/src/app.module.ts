675f87430b5ce42c4e6f46fe86ffe92f2003b52b
EmerLopez <emersonlhdz2020@gmail.com>
2026-05-05 16:05:21 -0600

    T-02  Configurar variables de entorno con @nestjs/config

--- backend/ecommerce/src/app.module.ts
+++ backend/ecommerce/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, ProductsModule, CartModule],
+  imports: [
+    UsersModule, ProductsModule, CartModule, AuthModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
