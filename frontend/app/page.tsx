09866efbe7a055ab3b41928e2589e891f70edba5
SnowLeopardt <xquickopex2017@gmail.com>
2026-05-05 21:08:20 -0600

    T-01 Crear frontend con Next.js TypeScript y Tailwind CSS

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl text-blue-500 font-bold">
+        Prueba de Tailwind
+      </h1>
+    </div>
+  );
+}
