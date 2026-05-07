a9c0abdbac1cfa9d46f7edf5a345eca00f45f948
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-05-06 20:29:03 -0600

    feat: create user, product, cart and auth modules structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { CartModule } from './cart/cart.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -26,6 +30,14 @@ import { AppService } from './app.service';
         logging: config.get('NODE_ENV') === 'development',
       }),
     }),
+
+    ProductsModule,
+
+    CartModule,
+
+    UsersModule,
+
+    AuthModule,
   ],
   controllers: [AppController],  // ← esto es clave
   providers: [AppService],
---
+++ backend/src/database/data-source.ts
@@ -0,0 +1,23 @@
+
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();  
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host:     process.env.DB_HOST,
+  port:     Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+
+ 
+  entities: ['src/**/*.entity.ts'],
+
+
+  migrations: ['src/database/migrations/*.ts'],
+
+
+  synchronize: false,
+});
