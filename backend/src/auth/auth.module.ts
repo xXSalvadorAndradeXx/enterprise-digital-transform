c108318848d2ee9ca75d68f3e7fdeac8fa98f830
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-05-06 23:53:43 -0600

    `fix(auth): configurar TypeORM y JwtModule en AuthModule`

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
+import { User } from '../users/entities/user.entity';
+import { Cart } from '../cart/entities/cart.entity';
+
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Cart]),
+
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
