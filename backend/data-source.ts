b7a174355c3b1858dfe06ff5aaa51aba8c6f3f5a
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-05-07 20:26:59 -0600

    T6 - completada migracion faltante

---
+++ backend/data-source.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+});
