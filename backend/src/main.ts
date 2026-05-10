cb6c6c0fb045f06a9e000df48ef3af1998ad93c7
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-05-09 20:37:35 -0600

    Implementacion de Swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   // Crear app
@@ -10,7 +11,7 @@ async function bootstrap() {
   // Configuración CORS
   app.enableCors({
     origin: [
-      'http://localhost:3001',
+      'http://localhost:3001', // Frontend React
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
@@ -23,19 +24,31 @@ async function bootstrap() {
   // Validaciones globales
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,           // elimina campos no permitidos
-      forbidNonWhitelisted: true, // lanza error si mandan campos extra
-      transform: true,            // transforma tipos automáticamente
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
   // Filtro global de errores
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  // Levantar servidor
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Tienda API')
+    .setDescription('API REST del backend')
+    .setVersion('1.0')
+    .addBearerAuth() // para JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // Puerto
   await app.listen(3000);
 
   console.log('API corriendo en: http://localhost:3000/api');
+  console.log('Docs Swagger: http://localhost:3000/api/docs');
 }
 
 bootstrap();
