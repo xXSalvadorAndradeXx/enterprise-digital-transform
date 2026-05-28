3f67468b3472f9ed98cc1b0df143805b1f4349de
xXSalvadorAndradeXx <salvadorandrade7089@gmail.com>
2026-05-27 23:49:40 -0600

    Limpieza

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl text-blue-500 font-bold">
-        Prueba de Tailwind
-      </h1>
-    </div>
-  );
-}
