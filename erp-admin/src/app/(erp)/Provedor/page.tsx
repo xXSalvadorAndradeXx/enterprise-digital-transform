498f7f228b1e2becf6c2f3a5f18002d5c31060bf
SnowLeopardt <xquickopex2017@gmail.com>
2026-07-21 23:55:51 -0600

    TASK 665 T-02: Construcción de la interfaz de listado de Proveedores

--- erp-admin/src/app/(erp)/Provedor/page.tsx
+++ erp-admin/src/app/(erp)/Provedor/page.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import EmptyState from "@/components/ui/EmptyState";
+import ErrorState from "@/components/ui/ErrorState";
+
+import Image from "next/image";
+
+import EmptyIcon from "@/assets/images/bandeja.png";
+import ErrorIcon from "@/assets/images/adver.png";
+
 import { useState } from "react";
 
 import SearchBar from "@/components/ui/SearchBar";
@@ -9,6 +17,8 @@ import Pagination from "@/components/ui/Pagination";
 export default function ProveedorPage() {
   const [search, setSearch] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
+
+  const view: "table" | "empty" | "error" = "table";
 
   const columns = [
     {
@@ -64,7 +74,7 @@ export default function ProveedorPage() {
     w-[210px]
     rounded-[4px]
     border
-    border-[#0b07e6]
+    border-[#d3d3ed]
     bg-white
     px-4
     text-[14px]
@@ -72,9 +82,9 @@ export default function ProveedorPage() {
     text-[#6B7280]
     outline-none
     transition-all
-    focus:border-[#1e14d6]
+    focus:border-[#a9a6e4]
     focus:ring-1
-    focus:ring-[#1f14f0]
+    focus:ring-[#b8b7d4]
   "
 >
           <option>Filtrar por empresa</option>
@@ -116,17 +126,57 @@ export default function ProveedorPage() {
 
         </div>
 
-        <Table
-          columns={columns}
-          data={providers}
-        />
+       {view === "table" && (
+  <>
+    <Table
+      columns={columns}
+      data={providers}
+      showBorder={false}
+    />
 
-        <Pagination
-          currentPage={currentPage}
-          totalPages={24}
-          onPageChange={setCurrentPage}
-          className="mt-5"
-        />
+    <Pagination
+  currentPage={currentPage}
+  totalPages={24}
+  onPageChange={setCurrentPage}
+  className="mt-6"
+/>
+  </>
+)}
+
+{view === "empty" && (
+  <EmptyState
+    image={
+      <Image
+        src={EmptyIcon}
+        alt="Sin proveedores"
+        width={120}
+        height={120}
+      />
+    }
+    title="No hay proveedores"
+    description="Aún no existen proveedores registrados."
+    helperText="Presiona el botón Agregar proveedor para comenzar."
+    buttonText="Agregar proveedor"
+    onButtonClick={() => {}}
+  />
+)}
+
+{view === "error" && (
+  <ErrorState
+    image={
+      <Image
+        src={ErrorIcon}
+        alt="Error"
+        width={120}
+        height={120}
+      />
+    }
+    title="Ocurrió un error"
+    description="No fue posible cargar los proveedores."
+    buttonText="Reintentar"
+    onRetry={() => {}}
+  />
+)}
 
       </div>
 
